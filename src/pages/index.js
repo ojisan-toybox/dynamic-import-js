b0676ad877b62c1c388fa45ee8914c31679f3989
sadness_ojisan <sadness.ojisan@gmail.com>
2021-11-01 11:58:02 +0900

    success load esm

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,11 @@
+import { useEffect } from "react";
+
 export default () => {
+  useEffect(() => {
+    import("../modules/esm").then((mod) => {
+      console.log(mod);
+    });
+  }, []);
+
   return <div>hello</div>;
 };
